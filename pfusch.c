f0f718b4a218269d49e972aa9854a907d89d2b8c
JulianSteini <206629@fhwn.ac.at>
2025-04-10 10:29:31 +0200

    v1

--- pfusch.c
+++ pfusch.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(void)
+typedef struct command_line_arguments
 {
-    printf("Hello, Kevin!\n");
+    int i;
+    const char *s;
+    bool b;
+} cli_args;
+
+cli_args parse_command_line(int const argc, char *argv[])
+{
+    cli_args args = {0, NULL, false};
+
+    int optgot = -1;
+
+    do
+    {
+        optgot = getopt(argc, argv, "i:s:b");
+        switch(optgot)
+        {
+            case 'i':
+                args.i = atoi(optarg);
+                break;
+            case 's':
+                args.s = optarg;
+                break;
+            case 'b':
+                args.b = true;
+                break;
+            case '?':
+                printf("Usage: %s -i <number> -s <string> -b\n", argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        } while (optgot != -1);
+
+        if(args.i <= 0 || strlen(args.s) <5)
+        {
+            printf("\nusage: i: %d, s: %s, b: %d\n", args.i, args.s, args.b);
+            exit(EXIT_FAILURE);
+        }
+}
+
+int main(int argc, char *argv[])
+{
+    cli_args const args = parse_command_line(argc, argv);
+    printf("\ni: %d, s: %s, b: %d\n", args.i, args.s, args.b);
 
     return 0;
 }
