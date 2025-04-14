cae69075be21b88adcadcdd5e2d92b2f91cecc1b
JulianSteini <206629@fhwn.ac.at>
2025-04-14 22:12:58 +0200

    Neuer Ordner Test

---
+++ Protokoll + TaskVentilator/TaskVentilator_JulianSteinkellner.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+#include <fcntl.h>
